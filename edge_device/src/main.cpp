0bfebee544fa9b6c4b046cf0410c198e92062e3f
zacke00 <89964509+zacke00@users.noreply.github.com>
2023-04-18 16:06:07 +0200

    testing pio run

--- edge_device/src/main.cpp
+++ edge_device/src/main.cpp
@@ -1,9 +1,114 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_SHT31.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+//------------------------------------------------------------
+//SHT31 humidity and temperature sensor
+bool enableHeater = false;
+
+uint8_t loopCnt = 0;
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+//------------------------------------------------------------
+
+//------------------------------------------------------------
+//Wifi
+const char * home = "Telenor2437tal";
+const char * homepass = "pgvfheslvafvk";
+
+const char * ssid = "Student";
+const char * password = "kristiania1914";
+//------------------------------------------------------------
+
+//------------------------------------------------------------
+//connection to API
+const char * api_host = "10.0.0.11";
+const int api_port = 3000;
+const char* api_endpoint = "/";
+//------------------------------------------------------------
+
+//------------------------------------------------------------
+// LED pin
+const int LED_PIN = LED_BUILTIN;
+//------------------------------------------------------------
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+
+//------------------------------------------------------------
+//SHT31
+  while (!Serial)
+    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("SHT31 test");
+  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+
+  Serial.print("Heater Enabled State: ");
+  if (sht31.isHeaterEnabled())
+    Serial.println("ENABLED");
+  else
+    Serial.println("DISABLED");
+//------------------------------------------------------------
+
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  float Temperature_t = sht31.readTemperature();
+  float Humidity_h = sht31.readHumidity();
+
+  if (! isnan(Temperature_t)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.print(Temperature_t); Serial.print("\t\t");
+  } else { 
+    Serial.println("Failed to read temperature");
+  }
+  
+  if (! isnan(Humidity_h)) {  // check if 'is not a number'
+    Serial.print("Hum. % = "); Serial.println(Humidity_h);
+  } else { 
+    Serial.println("Failed to read humidity");
+  }
+
+  delay(1000);
+
+  // Toggle heater enabled state every 30 seconds
+  // An ~3.0 degC temperature increase can be noted when heater is enabled
+  if (loopCnt >= 30) {
+    enableHeater = !enableHeater;
+    sht31.heater(enableHeater);
+    Serial.print("Heater Enabled State: ");
+    if (sht31.isHeaterEnabled())
+      Serial.println("ENABLED");
+    else
+      Serial.println("DISABLED");
+
+    loopCnt = 0;
+  }
+  loopCnt++;
 }
+
+
+void connectToWiFi(const char * ssid, const char * pwd)
+{
+  Serial.println("Connecting to WiFi network: " + String(ssid));
+  WiFi.begin(ssid, pwd); // start connecting to the wifi network
+
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    // Blink LED while we're connecting:
+    digitalWrite( LED_PIN, !digitalRead(LED_PIN) );
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("Connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
